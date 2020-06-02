40dbd0f094a992d603fc53a33954467c6a4fdd53
Mesquita98 <57418232+Mesquita98@users.noreply.github.com>
2020-06-02 19:40:24 +0100

    dfd

--- js/scripts.js
+++ js/scripts.js
@@ -532,15 +532,4 @@ function setTextoTop(s) {
     return true;
   }
 
-  var myDropzone = new Dropzone("div#my-awesome-dropzone", { url: "/index.html#"});
-// "myAwesomeDropzone" is the camelized version of the HTML element's ID
-Dropzone.options.myAwesomeDropzone = {
-  paramName: "file", // The name that will be used to transfer the file
-  maxFilesize: 2, // MB
-  accept: function(file, done) {
-    if (file.name == "justinbieber.jpg") {
-      done("Naha, you don't.");
-    }
-    else { done(); }
-  }
-};
+
